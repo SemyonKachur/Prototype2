54e735a7abe0d144341304e38f498d3dd377569d
Semyon Kachur <57587302+SemyonKachur@users.noreply.github.com>
2021-04-29 12:25:45 +0500

    Add files via upload

---
+++ Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    private float _topBound = 30;
+    private float _lowerBound = -10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.z > _topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (gameObject.transform.position.z < _lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefab;
+    public int animalIndex;
+    private float _spawnRangeX = 20;
+    private float _spawnPosZ = 20;
+    private float _startDelay = 2;
+    private float _spawnInterval = 1.5f;
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", _startDelay, _spawnInterval); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+    private void SpawnRandomAnimal()
+    {
+            animalIndex = Random.Range(0, animalPrefab.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnPosZ);
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+    }
+}
